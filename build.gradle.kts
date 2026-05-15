0cb05229d58091f1248c389a38fd83238fbfa64d
Selim Mert Kastan <kastanselimmert_07@hotmail.com>
2026-05-15 12:20:59 +0200

    fix: increase security level

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,7 +49,7 @@ subprojects {
 configure<org.owasp.dependencycheck.gradle.extension.DependencyCheckExtension> {
         outputDirectory = owaspRoot
         format = org.owasp.dependencycheck.reporting.ReportGenerator.Format.ALL.toString()
-        failBuildOnCVSS = 3.0f
+        failBuildOnCVSS = 7.0f
         nvd.apiKey = System.getenv("NVD_API_KEY") ?: env.fetchOrNull("NVD_API_KEY") ?: ""
 
         data {
